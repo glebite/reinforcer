5c964a9bb794b809eb93e26f20e9d15ac972b533
glebite <glebite@gmail.com>
2023-02-14 20:37:22 -0500

    Added in subwords

--- src/miangin.py
+++ src/miangin.py
@@ -10,11 +10,13 @@ class Miangin:
     def __init__(self, filename):
         self.filename = filename
         self.organized = defaultdict()
+        self.words = []
 
     def read(self):
         with open(self.filename, 'r') as fp:
             for word in fp:
                 word = fp.readline().strip()
+                self.words.append(word)
                 first = word[0]
                 if first in self.organized:
                     self.organized[first].append(word)
@@ -38,8 +40,23 @@ class Miangin:
             res = max(self.organized[letter], key=len)
             print(f'{letter=} ==> {res} ==> {len(res)}')
 
+    def find_subwords(self):
+        words = self.words
+        subwords = {}
+        for word in words:
+            for i in range(len(word)):
+                for j in range(i+1, len(word)+1):
+                    subword = word[i:j]
+                    if subword != word and subword in words and subword not in subwords:
+                        subwords[subword] = set()
+                        for w in words:
+                            if subword in w:
+                                subwords[subword].add(w)
+        return subwords
+
 
 if __name__ == "__main__":
     x = Miangin(FILE_PATH)
     x.read()
     x.biggest()
+    print(x.find_subwords())
