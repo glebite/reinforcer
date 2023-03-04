77b95e99c5ae29cf86b3023ee861a24e073a1690
glebite <glebite@gmail.com>
2023-03-03 22:25:13 -0500

    Making some new changes...

--- src/grammar.py
+++ src/grammar.py
@@ -1,3 +1,16 @@
 """
 Drop 'ن' from the verb + add stuff
 """
+
+
+class Word:
+    """
+    """
+    def __init__(self):
+        """
+        """
+        pass
+
+
+if __name__ == "__main__":
+    pass
